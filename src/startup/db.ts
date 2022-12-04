f48dded6dda504547350e9e688bc08b9c2f232ee
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2022-12-04 19:21:13 +0530

    Update db.ts

--- src/startup/db.ts
+++ src/startup/db.ts
@@ -5,9 +5,13 @@ require('dotenv').config();
 const startDB = async () => {
   try {
     const db = "mongodb+srv://nishu:astro@nodenishu.tl2ymec.mongodb.net/bill?retryWrites=true&w=majority";
-    mongoose.connect(db).then(() => {
-      console.log("Connected to database");
-    });
+    
+    const con=await mongoose.connect(db,
+    {
+      useNewUrlParser:true,
+		  useUnifiedTopology:true,
+    })
+    console.log(`MongoDB Connected:${con.connection.host}`);
   } catch (err) {
     console.error(err);
   }

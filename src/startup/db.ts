1f7903a6d37c0e3c5a01b55e9c002d8d49767e44
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2022-12-04 16:42:35 +0530

    Update db.ts

--- src/startup/db.ts
+++ src/startup/db.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const startDB = async () => {
   try {
-    const db = process.env.MONGODB_URL;
+    const db = "mongodb+srv://nishu:astro@nodenishu.tl2ymec.mongodb.net/bill?retryWrites=true&w=majority";
     mongoose.connect(db).then(() => {
       console.log("Connected to database");
     });
